b14d643e2ab5cc5c7e7e6f0b0c7f6d0e89d878b3
allanschramm <3208077+allanschramm@users.noreply.github.com>
2023-03-01 21:23:47 -0300

    20230301

--- src/app/classes/forum.ts
+++ src/app/classes/forum.ts
@@ -3,56 +3,26 @@ export class Forum {
     public id?: number;
 
     constructor(){
-        this._titulo = '';
-        this._descricao = '';
-        this._data = '';
-        this._responsavel = '';
-        this._telefone = '';
+        this.titulo = '';
+        this.descricao = '';
+        this.data = '';
+        this.responsavel = '';
+        this.telefone = '';
     }
     
     // propriedade titulo
-    private _titulo : string;
-    public get titulo() : string {
-        return this._titulo;
-    }
-    public set titulo(v : string) {
-        this._titulo = v;
-    }
+    public titulo : string;    
 
     // propriedade descricao
-    private _descricao : string;
-    public get descricao() : string {
-        return this._descricao;
-    }
-    public set descricao(v : string) {
-        this._descricao = v;
-    }
+    public descricao : string;    
     
     // propriedade data
-    private _data : string;
-    public get data() : string {
-        return this._data;
-    }
-    public set data(v : string) {
-        this._data = v;
-    }
+    public data : string;    
     
     // propriedade responsavel
-    private _responsavel : string;
-    public get responsavel() : string {
-        return this._responsavel;
-    }
-    public set responsavel(v : string) {
-        this._responsavel = v;
-    }
+    public responsavel : string;    
     
     // propriedade telefone    
-    private _telefone : string;
-    public get telefone() : string {
-        return this._telefone;
-    }
-    public set telefone(v : string) {
-        this._telefone = v;
-    }
+    public telefone : string;    
     
 }
